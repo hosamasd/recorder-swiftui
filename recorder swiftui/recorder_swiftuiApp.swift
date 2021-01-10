d162bf296532603791ac4e60058269f08f4b7c75
hosam <hosamelmalt@gmail.com>
2021-01-10 12:20:11 +0200

    Initial Commit

---
+++ recorder swiftui/recorder_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  recorder_swiftuiApp.swift
+//  recorder swiftui
+//
+//  Created by hosam on 1/10/21.
+//
+
+import SwiftUI
+
+@main
+struct recorder_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
